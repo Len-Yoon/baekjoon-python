3f39efbd91e6ff55f3744229758442aacf63a670
윤성헌 <76799034+Len-Yoon@users.noreply.github.com>
2025-04-01 10:44:20 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 9.33 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -0,0 +1,12 @@
+def solution(str_list):
+    
+    for i in range(len(str_list)):
+        if str_list[i] == "l":
+            return str_list[:i]
+            break
+        elif str_list[i] == "r":
+            return str_list[i+1:]
+            break
+            
+    
+    return []
