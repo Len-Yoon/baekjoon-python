3264cb442450707834cd5de8737e8503917a7c8d
윤성헌 <76799034+Len-Yoon@users.noreply.github.com>
2025-04-14 16:59:06 +0900

    [level 1] Title: 폰켓몬, Time: 0.74 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,11 +1,6 @@
-import collections
 def solution(nums):
-    myNum = len(nums)//2
-
-    countDict = dict(collections.Counter(nums))
-
-    if (len(countDict) >= myNum):
-        return myNum
-    else : 
-        return len(countDict)
+    answer = len(set(nums))
     
+    if answer > len(nums)//2:
+        return len(nums)//2
+    return answer
